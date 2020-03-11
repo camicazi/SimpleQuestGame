b8b332a0294a93d59edde0fca9698e37ca3ad05d
Anton Strand <a.strand@luukku.com>
2020-03-11 18:37:40 +0200

    Added scripts to coin rotation and collision
    Should react to collision with player controller

---
+++ Simple Quest game/Assets/Scenes/YellowCoinCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowCoinCollision: MonoBehaviour
+{
+    public GameObject YellowCoin;
+    void OnCollisionEnter(Collision other)
+    {
+        
+        if (other.gameObject.CompareTag("RightHand"))
+        {
+            float innerRadius = 0.3f;
+            float outerRadius = 2f;
+
+           // this.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+            var localOffset = new Vector3(Random.Range(-10f, 10f), Random.Range(0f, 10f), Random.Range(-10f, 10f));
+            var wordOffset = transform.rotation * localOffset;
+            var position = transform.position + wordOffset;
+            Instantiate(YellowCoin, position, transform.rotation);
+            FindObjectOfType<ScoreCounter>().Score++;
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            var localOffset = new Vector3(Random.Range(-10f, 10f), Random.Range(0f, 10f), Random.Range(-10f, 10f));
+            var wordOffset = transform.rotation * localOffset;
+            var position = transform.position + wordOffset;
+            Instantiate(YellowCoin, position, transform.rotation);
+            Destroy(this.gameObject);
+
+
+        }
+    }
+}
