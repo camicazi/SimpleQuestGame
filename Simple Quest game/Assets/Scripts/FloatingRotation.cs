55d267f06e256b191b6835a8cf477c9e4b1ac955
Anton Strand <a.strand@luukku.com>
2020-03-11 18:39:35 +0200

    Added some more scripts WORK IN PROGRESS

---
+++ Simple Quest game/Assets/Scripts/FloatingRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingRotation : MonoBehaviour
+{
+    public Transform tran;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tran.Rotate(26.6f*Time.deltaTime, 11.2f*Time.deltaTime, 16f*Time.deltaTime, Space.Self);
+    }
+}
---
+++ Simple Quest game/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public static Timer Instance;
+    public float remainingTime = 15.0f;
+    public bool gamePaused = true;
+    
+
+    void Update()
+    {
+        // The timer should only be active when the game is not paused
+        if (!gamePaused) {
+            remainingTime -= Time.deltaTime;
+        }
+
+    }
+    void Awake() {
+        Instance = this;
+
+    }
+    //functio for pausigg the timer
+    public void pauseGame()
+    {
+        gamePaused = true;
+    }
+    //functio for resuming the timer
+    public void resumeGame()
+    {
+        gamePaused = false;
+    }
+
+
+    // starting a new game should reset the timer
+    public void newGame()
+    {
+        remainingTime = 15.0f;
+        gamePaused = false;
+    }
+}
