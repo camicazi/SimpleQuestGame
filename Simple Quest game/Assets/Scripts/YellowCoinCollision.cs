ac8d15c9adaafef92cef3fca4db9bdd3b3d6a305
Anton Strand <a.strand@luukku.com>
2020-03-15 18:01:03 +0200

    Updates to yellowcoincollision
    Added rand function to make it possible to get negative or positive number while still being a minimum distance from zero.
    removed unnecessary local/world offset.Position is enough.
    Added libe for ncreasing score in an ondestroy function.

--- Simple Quest game/Assets/Scripts/YellowCoinCollision.cs
+++ Simple Quest game/Assets/Scripts/YellowCoinCollision.cs
@@ -5,32 +5,39 @@ using UnityEngine;
 public class YellowCoinCollision: MonoBehaviour
 {
     public GameObject YellowCoin;
+
+    public void OnDestroy()
+    {
+        ScoreCounter.Instance.Score++;
+    }
+
     void OnCollisionEnter(Collision other)
     {
         
         if (other.gameObject.CompareTag("RightHand"))
         {
-            float innerRadius = 0.3f;
-            float outerRadius = 2f;
-
-           // this.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
-            var localOffset = new Vector3(Random.Range(-10f, 10f), Random.Range(0f, 10f), Random.Range(-10f, 10f));
-            var wordOffset = transform.rotation * localOffset;
-            var position = transform.position + wordOffset;
-            Instantiate(YellowCoin, position, transform.rotation);
-            FindObjectOfType<ScoreCounter>().Score++;
+            //this.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+            var localOffset = new Vector3(this.transform.position.x + Rand(Random.Range(0.05f, 0.3f)), Random.Range(0.2f, 1.5f), this.transform.position.z + Rand(Random.Range(0.05f, 0.3f)));
+            Instantiate(YellowCoin, localOffset, transform.rotation);
             Destroy(this.gameObject);
         }
         else
         {
-            this.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
-            var localOffset = new Vector3(Random.Range(-10f, 10f), Random.Range(0f, 10f), Random.Range(-10f, 10f));
-            var wordOffset = transform.rotation * localOffset;
-            var position = transform.position + wordOffset;
-            Instantiate(YellowCoin, position, transform.rotation);
+            //this.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            var localOffset = new Vector3(this.transform.position.x + Rand(Random.Range(0.05f, 0.3f)), Random.Range(0.2f, 1.5f), this.transform.position.z + Rand(Random.Range(0.05f, 0.3f)));
+            Instantiate(YellowCoin, localOffset, transform.rotation);
             Destroy(this.gameObject);
 
-
+        }
+    }
+    float Rand(float randnum)
+    {
+        if(Random.Range(0, 100)>=50){
+            return randnum;
+        }
+        else
+        {
+            return randnum * -1f;
         }
     }
 }
