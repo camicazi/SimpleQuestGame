83def99fa838eae9db0452c2650cb00eb12d5c67
Anton Strand <a.strand@luukku.com>
2020-03-15 21:53:13 +0200

    Cleaned up YellowCoin Collision
    Now has the tags it compares to in list form.
    Commented code

--- Simple Quest game/Assets/Scripts/YellowCoinCollision.cs
+++ Simple Quest game/Assets/Scripts/YellowCoinCollision.cs
@@ -5,34 +5,29 @@ using UnityEngine;
 public class YellowCoinCollision: MonoBehaviour
 {
     public GameObject YellowCoin;
-
+    List<string> HandTags = new List<string>() { "LeftHand", "RightHand" };
+    //When object is desotryed because of collision, increase score by one
     public void OnDestroy()
     {
         ScoreCounter.Instance.Score++;
     }
 
+    //On collision, instantiate another one in a new position with minimum distance 5cm and maximum 30cm from former position
+    //lastly destroy the old instannce
     void OnCollisionEnter(Collision other)
     {
-        
-        if (other.gameObject.CompareTag("RightHand"))
+        if ( HandTags.Contains(other.gameObject.tag))
         {
-            //this.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
-            var localOffset = new Vector3(this.transform.position.x + Rand(Random.Range(0.05f, 0.3f)), Random.Range(0.2f, 1.5f), this.transform.position.z + Rand(Random.Range(0.05f, 0.3f)));
-            Instantiate(YellowCoin, localOffset, transform.rotation);
+            var position = new Vector3(this.transform.position.x + Rand(Random.Range(0.05f, 0.3f)), Random.Range(0.2f, 1.5f), this.transform.position.z + Rand(Random.Range(0.05f, 0.3f)));
+            Instantiate(YellowCoin, position, transform.rotation);
             Destroy(this.gameObject);
-        }
-        else
-        {
-            //this.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
-            var localOffset = new Vector3(this.transform.position.x + Rand(Random.Range(0.05f, 0.3f)), Random.Range(0.2f, 1.5f), this.transform.position.z + Rand(Random.Range(0.05f, 0.3f)));
-            Instantiate(YellowCoin, localOffset, transform.rotation);
-            Destroy(this.gameObject);
-
         }
     }
+    // Set 50% chance the random range will be negative in order to not have the object move only in positive directions during instantiation.
     float Rand(float randnum)
     {
-        if(Random.Range(0, 100)>=50){
+        if(Random.Range(0, 100)>=50)
+        {
             return randnum;
         }
         else
