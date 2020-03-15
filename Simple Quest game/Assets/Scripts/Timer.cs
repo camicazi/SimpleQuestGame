497daf6beab7d7c1b09132970f75cb13c1d986ef
Anton Strand <a.strand@luukku.com>
2020-03-15 21:33:19 +0200

    Updated timer.cs
    Now removes any remaining coins when the time runs out.
    Activates finalScore text in main menu once the first game starts.
    Commented code.

--- Simple Quest game/Assets/Scripts/Timer.cs
+++ Simple Quest game/Assets/Scripts/Timer.cs
@@ -16,34 +16,43 @@ public class Timer : MonoBehaviour
     void Update()
     {
         // The timer should only be active when the game is not paused. Game unpauses once clicking "new game"
-        // WHen timer reaches zero, you return to the main menu and show final score
-        if (!gamePaused) {
+        // When timer reaches zero, you return to the main menu, destroy remaining yellow coins, and show final score
+        //while counting down the timer shows the remaining time in int form on the linked timerDisplay canvas
+        if (!gamePaused)
+        {
             remainingTime -= Time.deltaTime;
         }
         if (remainingTime <= 0)
         {
+            var gameObjects = GameObject.FindGameObjectsWithTag("YellowCoin");
+            for (var i = 0; i < gameObjects.Length; i++)
+            {
+                Destroy(gameObjects[i]);
+            }
             remainingTime = 0;
-            menu.SetActive(true);
             menuScore.SetActive(true);
-            menuScore.GetComponent<Text>().text = "Final score: " + score;
+            menuScore.GetComponent<Text>().text = "Final score: " + ScoreCounter.Instance.Score;
+            menu.SetActive(true);
         }
         timerDisplay.GetComponent<Text>().text = "Time left: " + remainingTime.ToString("N0");
 
 
     }
     // use awake instead of start to make sure this is only run once during the whole game session
-    void Awake() {
+    void Awake()
+    {
         Instance = this;
-        gamePaused = true; // game should start with main menu open, so the timer should be paused
+        gamePaused = true; // game starts with main menu open, so the timer should be paused
 
     }
 
 
-    // starting a new game should reset the timer
+    // starting a new game should reset the timer and score counter and activate the time countdown
     public void NewGame()
     {
         timerDisplay.SetActive(true);
         remainingTime = 15.0f;
         gamePaused = false;
+        ScoreCounter.Instance.Score = 0;
     }
 }
