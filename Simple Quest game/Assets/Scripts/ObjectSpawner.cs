e41320603d1287cd632f5dced2f3aa8e89bb477c
Anton Strand <a.strand@luukku.com>
2020-03-15 21:54:05 +0200

    Created ObjectSpawner script
    Is used for spawning yellowCoin prefab once newgame button is pressed

---
+++ Simple Quest game/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script that spawns any object to specific position. In this case only used for the yellow coin
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject ObjectoToSpawn;
+    public void SpawnObject()
+    {
+        var Position = new Vector3(0.0f, 1.24f, 1.63f);
+        Instantiate(ObjectoToSpawn, Position, Quaternion.identity);
+
+    }
+
+}
