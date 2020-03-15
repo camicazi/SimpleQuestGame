9e6881dabf30b78957e3ee47926299e2ba0f2d4d
Anton Strand <a.strand@luukku.com>
2020-03-15 18:03:39 +0200

    updated scorecounter script, still not working quite as it should
    Either score not increased when it should, or score not displaying correctly in linked objects.

--- Simple Quest game/Assets/Scripts/ScoreCounter.cs
+++ Simple Quest game/Assets/Scripts/ScoreCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class ScoreCounter : MonoBehaviour
     private static ScoreCounter m_Instance;
     public int Score { get { return m_Score; } set { m_Score = value; } }
     public static ScoreCounter Instance { get { return m_Instance; } }
+    public Text scoreText;
 
     public void Start()
     {
@@ -16,5 +18,11 @@ public class ScoreCounter : MonoBehaviour
             return;
         }
         m_Instance = this;
+
+        scoreText.text = "Score: " + Score.ToString();
+    }
+    public void Update()
+    {
+        scoreText.text = "Score: " + Score.ToString();
     }
 }
