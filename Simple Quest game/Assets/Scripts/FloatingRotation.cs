349406503854135e64e170035d2ee773618e2602
Anton Strand <a.strand@luukku.com>
2020-03-18 21:35:46 +0200

    floatingrotation.cs, Fix typo in comment

--- Simple Quest game/Assets/Scripts/FloatingRotation.cs
+++ Simple Quest game/Assets/Scripts/FloatingRotation.cs
@@ -6,7 +6,7 @@ public class FloatingRotation : MonoBehaviour
 {
     public Transform tran;
 
-    // Makes object rotate slowly in the air without moving 
+    // Makes linked object rotate slowly in the air without moving 
     void Update()
     {
         tran.Rotate(26.6f*Time.deltaTime, 11.2f*Time.deltaTime, 16f*Time.deltaTime, Space.Self);
